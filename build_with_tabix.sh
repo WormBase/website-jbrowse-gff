2e5804c7c022aabce768b64a579094bec1d511a6
Scott Cain <scott+git@scottcain.net>
2021-05-04 20:56:09 +0000

    trying out the bgzip and tabix in the base image

---
+++ build_with_tabix.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# this is a simple test script for tabix indexing gff
+
+set -e
+
+RELEASE=280
+while getopts r:s:a:k: option
+do
+case "${option}"
+in
+r) 
+  RELEASE=${OPTARG}
+  ;;
+s) 
+  SPECIES=${OPTARG}
+  ;;
+a)
+  AWSACCESS=${OPTARG}
+  ;;
+k)
+  AWSSECRET=${OPTARG}
+  ;;
+esac
+done
+
+if [ -z "$RELEASE" ]
+then
+    RELEASE=${WB_RELEASE}
+fi
+
+if [ -z "$SPECIES" ]
+then
+    SPECIES=${WB_SPECIES}
+fi
+
+if [ -z "$AWSACCESS" ]
+then
+    AWSACCESS=${AWS_ACCESS_KEY}
+fi
+
+if [ -z "$AWSSECRET" ]
+then
+    AWSSECRET=${AWS_SECRET_KEY}
+fi
+
+if [ -z "$AWSBUCKET" ]
+then
+    if [ -z "${AWS_S3_BUCKET}" ]
+    then
+        AWSBUCKET=agrjbrowse
+    else
+        AWSBUCKET=${AWS_S3_BUCKET}
+    fi
+fi
+
+wget ftp://ftp.wormbase.org/pub/wormbase/releases/WS280/species/c_elegans/PRJNA13758/c_elegans.PRJNA13758.WS280.annotations.gff3.gz
+gzip -d c_elegans.PRJNA13758.WS280.annotations.gff3.gz
+
+gt gff3 -tidy -sortlines -retainids c_elegans.PRJNA13758.WS280.annotations.gff3 > worm.gff
+
+bzgip worm.gff
+tabix worm.gff.gz
+
+aws s3 cp --acl public-read worm.gff.gz s3://agrjbrowse/test/WS280/c_elegans_PRJNA13758/worm.gff.gz
+aws s3 cp --acl public-read worm.gff.gz.tbi s3://agrjbrowse/test/WS280/c_elegans_PRJNA13758/worm.gff.gz.tbi
+
+ 
+
+
