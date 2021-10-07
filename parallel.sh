3b7cf7ac593d26a61d23259c6b3b4b9caf312198
Scott Cain <scott+git@scottcain.net>
2021-10-07 22:48:16 +0000

    making ready for production (more cpus, all species)

--- parallel.sh
+++ parallel.sh
@@ -48,33 +48,33 @@ then
 fi
 
 SPECIESLIST=(
-#'t_muris_PRJEB126'
-#'c_elegans_PRJNA13758'
-#'p_pacificus_PRJNA12644'
-#'c_nigoni_PRJNA384657'
-#'b_malayi_PRJNA10729'
-#'c_angaria_PRJNA51225'
-#'c_remanei_PRJNA577507'
-#'c_brenneri_PRJNA20035'
-#'c_briggsae_PRJNA10731'
-#'c_elegans_PRJNA275000'
-#'c_japonica_PRJNA12591'
-#'c_remanei_PRJNA53967'
-#'c_sinica_PRJNA194557'
-#'c_tropicalis_PRJNA53597'
-#'o_volvulus_PRJEB513'
-#'p_redivivus_PRJNA186477'
-#'s_ratti_PRJEB125'
-#'c_elegans_PRJEB28388'
-#'c_inopinata_PRJDB5687'
-#'c_latens_PRJNA248912'
-#'o_tipulae_PRJEB15512'
-#'c_becei_PRJEB28243'
-#'c_bovis_PRJEB34497'
-#'c_panamensis_PRJEB28259'
-#'c_parvicauda_PRJEB12595'
-#'c_quiockensis_PRJEB11354'
-#'c_sulstoni_PRJEB12601'
+'t_muris_PRJEB126'
+'c_elegans_PRJNA13758'
+'p_pacificus_PRJNA12644'
+'c_nigoni_PRJNA384657'
+'b_malayi_PRJNA10729'
+'c_angaria_PRJNA51225'
+'c_remanei_PRJNA577507'
+'c_brenneri_PRJNA20035'
+'c_briggsae_PRJNA10731'
+'c_elegans_PRJNA275000'
+'c_japonica_PRJNA12591'
+'c_remanei_PRJNA53967'
+'c_sinica_PRJNA194557'
+'c_tropicalis_PRJNA53597'
+'o_volvulus_PRJEB513'
+'p_redivivus_PRJNA186477'
+'s_ratti_PRJEB125'
+'c_elegans_PRJEB28388'
+'c_inopinata_PRJDB5687'
+'c_latens_PRJNA248912'
+'o_tipulae_PRJEB15512'
+'c_becei_PRJEB28243'
+'c_bovis_PRJEB34497'
+'c_panamensis_PRJEB28259'
+'c_parvicauda_PRJEB12595'
+'c_quiockensis_PRJEB11354'
+'c_sulstoni_PRJEB12601'
 'c_tribulationis_PRJEB12608'
 'c_uteleia_PRJEB12600'
 'c_waitukubuli_PRJEB12602'
@@ -91,7 +91,7 @@ CONFPATH=/website-genome-browsers/jbrowse/conf/c_elegans.jbrowse.conf
 #this is by far the longest running portion of the script (typically a few hours)
 #$MAKEPATH $SKIPFLATFILE --conf $CONFPATH --quiet --species $SPECIES 2>1 | grep -v "Deep recursion"; mv 1 $LOGFILE
 echo "starting processing gff files"
-parallel -j 2 $MAKEPATH --quiet --conf $CONFPATH  --species {} ::: "${SPECIESLIST[@]}"
+parallel -j "95%" $MAKEPATH --quiet --conf $CONFPATH  --species {} ::: "${SPECIESLIST[@]}"
 
 INLINEINCLUDEPATH=/website-genome-browsers/jbrowse/bin/inline_includes.pl
 
@@ -113,7 +113,7 @@ UPLOADTOS3PATH=/agr_jbrowse_config/scripts/upload_to_S3.pl
 #REMOTEPATH="test/WS$RELEASE/$SPECIES"
 
 #$UPLOADTOS3PATH $ONLYTRACKLIST --bucket $AWSBUCKET --local "$SPECIES/" --remote $REMOTEPATH --AWSACCESS $AWSACCESS --AWSSECRET $AWSSECRET
-parallel -j 2 $UPLOADTOS3PATH --bucket $AWSBUCKET --local {1}"/" --remote "MOD-jbrowses/WormBase/WS$RELEASE/"{1} --AWSACCESS $AWSACCESS --AWSSECRET $AWSSECRET ::: "${SPECIESLIST[@]}"
+parallel -j "95%"  $UPLOADTOS3PATH --bucket $AWSBUCKET --local {1}"/" --remote "MOD-jbrowses/WormBase/WS$RELEASE/"{1} --AWSACCESS $AWSACCESS --AWSSECRET $AWSSECRET ::: "${SPECIESLIST[@]}"
  
 
 
